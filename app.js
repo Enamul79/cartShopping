788d128679c8b8b436e30776901c156c252b415e
Muhammad Enamul Haque <enahaque79@gmail.com>
2022-02-13 13:54:14 +0600

    updated

--- app.js
+++ app.js
@@ -1,15 +1,29 @@
+function updateCaseNumber(isIncresing){
+   const caseInput = document.getElementById('case-number');
+   const caseNumber = caseInput.value;
+   if(isIncresing == true){
+      caseInput.value = parseInt(caseNumber) + 1;
+   }else{
+      caseInput.value = parseInt(caseNumber) - 1;
+   }
+   
+}
+
+
 document.getElementById('case-plus').addEventListener('click',function(){
    // console.log('case-plus click')
 
-   const caseInput = document.getElementById('case-number');
-   const caseNumber = caseInput.value; // console.log(caseNumber);
-   caseInput.value = parseInt(caseNumber) + 1;
- 
+   // const caseInput = document.getElementById('case-number');
+   // const caseNumber = caseInput.value; // console.log(caseNumber);
+   // caseInput.value = parseInt(caseNumber) + 1;
+   
+   updateCaseNumber(true);
 
 })
 
 document.getElementById('case-minus').addEventListener('click',function(){
-   const caseInput = document.getElementById('case-number');
-   const caseNumber = caseInput.value; // console.log(caseNumber);
-   caseInput.value = parseInt(caseNumber) - 1;
+   // const caseInput = document.getElementById('case-number');
+   // const caseNumber = caseInput.value; // console.log(caseNumber);
+   // caseInput.value = parseInt(caseNumber) - 1;
+   updateCaseNumber(false);
 })
